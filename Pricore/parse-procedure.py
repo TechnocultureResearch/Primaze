b327e4f9222b9f3732bc59c04d3a6ee5ef0ab7f1
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-10-31 13:18:09 +0000

    procedure parser wip

--- Pricore/parse-procedure.py
+++ Pricore/parse-procedure.py
@@ -1,72 +1,111 @@
 import sys
-from yaml import load, dump
+from yaml import load, dump, FullLoader
 from pprint import pprint
 
 
-file_data = load("""
-name: Single Target PCR
-specie: Homeo Sapiens
-pipeline:
- - fetch_genome
- - gc_check_template
- - check_tm
-""")
+class Protocol:
+    file_name = ""
+    parsed_protocol = []
+    name = ""
+    specie = ""
+    procedure = None
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.parsed_protocol = Protocol.get_procedure_data(file_name)
+        self.procedure = Procedure(self.parsed_protocol)
+        
+    
+    def __repr__(self):
+        pretty_code = "\n{0}\n=== {1} ===\n{0}\n".format('='*(len(self.name) + 8), self.name)
+        pretty_code += dump(self.name, default_flow_style=False)
+        return pretty_code
 
 
-file_data1 = ("""
-name: Single Target PCR
-specie: Homeo Sapiens
-pipeline:
- - fetch_genome
- - gc_check_template
- - check_tm
-""")
+    @staticmethod
+    def get_procedure_data(file_name):
+        return load(
+            open(file_name),
+            Loader = FullLoader
+        )
 
 
-def print_protocol(file_data):
-    file_name = file_data['name']
-    pretty_code = "\n{0}\n==={1}===\n{0}\n".format(
-        '='*(len(file_name) + 6), file_name)
-    pretty_code += dump(file_data, default_flow_style=False)
-    print(pretty_code)
-    return pretty_code
+class Command:
+    name = ""
+    function = None
+
+    def __init__(self, name, f):
+        self.name = name
+        self.function = f
+    
+    def __repr__(self):
+        return self.name
+
+
+class Procedure:
+    raw_procedure = []
+    commands = {
+        Command('fetch_genome', fetch_genome),
+        Command('gc_check_template', gc_check_template)
+    }
+
+
+    def __init__(self, parsed_protocol):
+        try:
+            self.raw_procedure = parsed_protocol['procedure']
+        except KeyError as kErr:
+            print("FATAL: {}".format(kErr))
+            exit(1)
+        
+        self.validate_procedure()
+    
+
+    def validate_procedure(self):
+        try:
+            for _ in self.raw_procedure:
+                if _ not in self.commands: raise SyntaxError("Command: {} is not recognized.".format(_))
+        except SyntaxError as sErr:
+            print(sErr)
+            exit(1)
+
+
 
 
 def fetch_genome():
     data = ['A', 'B']
     print(data)
     return data
+    
 
-
-def gc_check_template():
+def gc_check_template(cls):
     return True
 
 
-commands = {
-    'fetch_genome': fetch_genome,
-    'gc_check_template': gc_check_template
-}
+# def validate_protocol(protocol):
+#     try:
+#         for _ in protocol:
+#             if _ not in commands: raise SyntaxError("Command: {} is not recognized.".format(_))
+#     except SyntaxError as sErr:
+#         print(sErr)
+#         exit(1)
 
 
-# pprint(file_data)
-# print(file_data['name'])
-
-
-def validate_protocol(protocol):
-    valid = True
+def runner(procedure_data):
     try:
-        for _ in protocol['pipeline']:
-            if _ not in commands:
-                raise SyntaxError("Command: {} is not recognized.".format(_))
-    except SyntaxError as sErr:
-        print(sErr, file=sys.stderr)
+        return [commands[f]() for f in procedure_data['pipeline']]
+    except KeyError as kErr:
+        print("FATAL: {}".format(kErr))
         exit(1)
 
 
-def runner(protocol):
-    return [commands[f]() for f in file_data['pipeline']]
+
+# commands = 
+
+# print_protocol(file_data)
+# validate_protocol(file_data)
+# runner(file_data)
 
 
-print_protocol(file_data)
-validate_protocol(file_data)
-runner(file_data)
+# procedure = get_procedure_data("data/procedure.yml")
+p = Protocol("data/procedure.yml")
+# print_protocol(procedure)
