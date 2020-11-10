18b9fcd26dd45c9309c2bbdacd11174e3d3220dd
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-10 16:36:06 +0530

    wip cleaning up download script

--- scripts/download_genome.py
+++ scripts/download_genome.py
@@ -23,17 +23,14 @@ class NcbiFtpHTMLParser(HTMLParser):
         self.end_tag = tag
 
     def handle_data(self, data):
-        if (
-            self.start_tag == self.end_tag and config("GENO_FOLD_IDENTIFIER") in data
-        ):  # HARDCODED
+        if self.start_tag == self.end_tag and config("GENO_FOLD_IDENTIFIER") in data:
             data = data.replace("/", "")
             if "p" in data.split(".")[-1]:
                 self.data.append(data)
 
     def get_parsed_list(self, data: str) -> List[str]:
         self.feed(data)
-        return self.data
-        # print((self.data))
+        return self.data  # print((self.data))
 
 
 chromosome_url = config("CHROMOSOME_URL")
@@ -53,61 +50,83 @@ def get_chromosome_names(url: str) -> List[str]:
     return parser.get_parsed_list(r.text)
 
 
+get_ch_num = lambda file_name: file_name.split("_genomic")[0].split("p")[-1]
+
+
+def file_name_from_url(url: str) -> str:
+    _ = url.split("//")[1].split("/")
+    file_name = _[-1]  # print(file_name)
+    return file_name
+
+
+def dir_name_from_url(url: str) -> str:
+    _ = url.split("//")[1].split("/")
+    del _[0]
+    del _[-1]
+    dir_name = "/".join(_) + "/"  # print(dir_name)
+    return dir_name
+
+
+def ftp_url_split(url: str) -> Tuple[str, str, str]:
+    file_name = file_name_from_url(url)
+    dir_name = dir_name_from_url(url)
+    ch_num = get_ch_num(file_name)
+    return (dir_name, file_name, ch_num)
+
+
+ftp_url = lambda urls: urls[0].split("//")[1].split("/")[0]
+
+
+def download_file_from_ftp(
+    ftp_url: str, data_folder: str, dir_name: str, file_name: str
+) -> None:
+    # https://titanwolf.org/Network/Articles/Article?AID=a5c53dad-13fb-40dc-a0dd-faed9011d054#gsc.tab=0
+    with FTP(ftp_url) as ftp:
+        ftp.login(passwd=config("EMAIL"))
+        ftp.cwd(dir_name)  # ftp.dir()
+
+        with open(data_folder + file_name, "wb") as d:
+            ftp.voidcmd("TYPE I")
+            total = ftp.size(file_name)  # print(size(total))
+            with tqdm(
+                total=total,
+                unit_scale=True,
+                # miniters=1,
+                leave=True,
+            ) as pbar:
+
+                def cb(data):
+                    pbar.update(len(data))
+                    d.write(data)
+
+                ftp.retrbinary("RETR {}".format(file_name), cb)
+
+
+print_download_title = lambda i, urls, file_name: print(
+    "{}/{} [{:.0f}%]: {}".format(i + 1, len(urls), (i + 1) / len(urls) * 100, file_name)
+)
+
+
 def download(urls: List[str]) -> None:
-    assert isinstance(urls, list)
-    # print(urls)
+    assert isinstance(urls, list)  # print(urls)
 
-    ftp_url = urls[0].split("//")[1].split("/")[0]
-    print(ftp_url)
-
-    def url_split(url: str) -> Tuple[str, str]:
-        _ = url.split("//")[1].split("/")
-        del _[0]
-        file_name = _[-1]
-        # print(file_name)
-
-        del _[-1]
-        dir = "/".join(_) + "/"
-        # print(dir)
-        return (dir, file_name)
+    _ftp_url = ftp_url(urls)  # print(_ftp_url)
 
     for i, url in enumerate(urls):
-        dir = url_split(url)[0]
-        file_name = url_split(url)[1]
+        (dir, file_name, chnum) = ftp_url_split(url)
 
-        print(
-            "{}/{} [{:.0f}%]: {}".format(
-                i + 1, len(urls), (i + 1) / len(urls) * 100, file_name
-            )
-        )
+        data_folder = config("DATA_FOLDER")
+        assert data_folder is not None
+        # assert
+
+        print_download_title(i, urls, file_name)
         try:
-            with FTP(ftp_url) as ftp:
-                ftp.login(passwd=config("EMAIL"))
-                ftp.cwd(dir)
-                # ftp.dir()
-
-                data_folder = config("DATA_FOLDER")
-                if file_name in listdir(data_folder):
-                    print("Already exists")
-                else:
-                    with open(data_folder + file_name, "wb") as d:
-                        ftp.voidcmd(
-                            "TYPE I"
-                        )  # https://titanwolf.org/Network/Articles/Article?AID=a5c53dad-13fb-40dc-a0dd-faed9011d054#gsc.tab=0
-                        total = ftp.size(file_name)
-                        # print(size(total))
-                        with tqdm(
-                            total=total,
-                            unit_scale=True,
-                            miniters=1,
-                            leave=True,
-                        ) as pbar:
-
-                            def cb(data):
-                                pbar.update(len(data))
-                                d.write(data)
-
-                            ftp.retrbinary("RETR {}".format(file_name), cb)
+            if file_name in listdir(data_folder):
+                print("Already exists")
+                break
+            else:
+                download_file_from_ftp(_ftp_url, data_folder, dir, file_name)
+                print("Checking Checksum...")
         except (error_temp, error_reply) as e:
             print(e)
             exit(1)
